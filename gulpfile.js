de6a6c3bf04830b95aa0df9694220a5da2d95ffc
Thuong Nguyen <thuongnh.uit@gmail.com>
2017-10-30 10:54:41 +0700

    Update gulpfile.js and fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var gutil = require('gulp-util');
 var cleanCSS = require('gulp-clean-css');
+var es = require('event-stream');
 
 var config = {
   bootstrapDir: './bower_components/bootstrap-sass',
   sourceDir: './css/**/*',
-  publicDir: './public'
+  publicDir: './public',
+  fontDir: './fonts/**/*'
 };
 
 gulp.task('css', function() {
@@ -13,6 +16,7 @@ gulp.task('css', function() {
          .pipe(sass({
            includePaths: [config.bootstrapDir + '/assets/stylesheets']
          }))
+         .on('error', gutil.log)
          .pipe(gulp.dest(config.publicDir + '/css'));
 });
 
@@ -21,17 +25,18 @@ gulp.task('prod-css', function() {
          .pipe(sass({
            includePaths: [config.bootstrapDir + '/assets/stylesheets']
          }))
+         .on('error', gutil.log)
          .pipe(cleanCSS({compatibility: 'ie8'}))
-         .pipe(gulp.dest(config.publicDir + '/css'));
+         .pipe(gulp.dest(config.publicDir + '/css'))
 });
 
 gulp.task('fonts', function() {
-  return gulp.src(config.bootstrapDir + '/assets/fonts/**/*')
+  return es.merge(gulp.src(config.bootstrapDir + '/assets/fonts/**/*'), gulp.src(config.fontDir))
          .pipe(gulp.dest(config.publicDir + '/fonts'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(config.sourceDir, ['prod-css']);
+  gulp.watch([config.sourceDir, config.fontDir], ['prod-css', 'fonts']);
 });
 
 gulp.task('default', ['css', 'fonts']);
